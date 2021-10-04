e2c999b8f3693605f00d4757e9257736d86d4d71
Ankit Tyagi <you@example.com>
2021-10-04 18:42:13 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const date = new Date();
+  const hour = date.getHours() * 60 * 60;
+  const minutes = date.getMinutes() * 60;
+  const seconds = date.getSeconds();
+
+  const totaltime = hour + minutes + seconds;
+  const prctime = (totaltime / 86400) * 100;
+  const getTime = Math.floor(prctime * 100) / 100;
+
+  const [currentTime, setCurrentTime] = useState(getTime);
+
+  const getcurrentTime = () => {
+    let date = new Date();
+    let hour = date.getHours() * 60 * 60;
+    let minutes = date.getMinutes() * 60;
+    let seconds = date.getSeconds();
+    let totaltime = hour + minutes + seconds;
+    let prctime = (totaltime / 86400) * 100;
+    let getTimeNew = Math.floor(prctime * 100) / 100;
+    setCurrentTime(getTimeNew);
+  };
+
+  setInterval(getcurrentTime, 100);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{currentTime + "%"}</h1>
+      <h2>You just can't beat the person who never gives up.</h2>
+      <h3>-Ankit Tyagi</h3>
     </div>
   );
-}
-
+};
 export default App;
